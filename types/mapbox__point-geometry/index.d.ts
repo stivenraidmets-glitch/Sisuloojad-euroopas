7bfd2b47b4eaef24e84f5457766df39e7a5aee6c
Stiven Raidmets <stivenraidmets@gmail.com>
2026-02-26 01:26:34 +0200

    Add local type stub for mapbox point-geometry (Vercel build)

---
+++ types/mapbox__point-geometry/index.d.ts
@@ -0,0 +1,8 @@
+/* Stub for mapbox__point-geometry (referenced by @types/mapbox-gl on Vercel) */
+declare module "@mapbox/point-geometry" {
+  export default class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number);
+  }
+}
